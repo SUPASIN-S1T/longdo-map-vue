61cb996132ac2fb056a66ec7deacbbaef129cf70
supasin.sa <supasin.sa@inet.co.th>
2024-09-10 23:37:14 +0700

    longdo-map-vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,76 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <longdo-map id="map" ref=map :zoom="15" :lastView="false" :fullscreen="true" @load="getCurrentLocation" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  comments: {},
+  watch: {
+    currentLocation() {
+      this.getDataLocation();
+    }
+  },
+  data() {
+    return {
+      currentLocation: null,
+    }
+  },
+  methods: {
+    getCurrentLocation(map) {
+      console.log('map :', map);
+      // REMOVE TOOLBAR
+      this.removeToolbar(map)
+      // GET CURRENT LOCATION (LAT,LON )
+      navigator.geolocation.getCurrentPosition((position) => {
+        const lat = position.coords.latitude;
+        const lon = position.coords.longitude;
+        this.currentLocation = { lat, lon };
+        console.log('this.currentLocation :', this.currentLocation);
+        // REDIRECT TO CURRENT LOCATION
+        map.location(this.currentLocation, true);
+        // CREATE MARKER
+        const marker = new window.longdo.Marker(this.currentLocation, { draggable: true, clickable: true });
+        map.Overlays.add(marker);
+        // UPDATE LOCATION (DROP MARKER)
+        this.updateLocation(marker);
+      }, (error) => {
+        console.log(error.message);
+      });
+    },
+    async getDataLocation() {
+      const { VUE_APP_MAP_URL, VUE_APP_API_KEY } = process.env;
+      const { data } = await this.axios.get(`${VUE_APP_MAP_URL}?lon=${this.currentLocation.lon}&lat=${this.currentLocation.lat}&key=${VUE_APP_API_KEY}`);
+      console.log('response data :', data);
+    },
+    removeToolbar(map) {
+      map.Ui.DPad.visible(false);
+      map.Ui.Scale.visible(false);
+      map.Ui.Toolbar.visible(false);
+      map.Ui.Zoombar.visible(false);
+      map.Ui.Crosshair.visible(false);
+      map.Ui.Geolocation.visible(false);
+      map.Ui.LayerSelector.visible(false);
+    },
+    updateLocation(marker) {
+      marker.event.bind('overlayDrop', (event) => {
+        this.currentLocation = event.location();
+      })
+    },
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.container {
+  height: 100vh;
+}
+
+#map {
+  width: 100%;
+  height: 100%;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import LongdoMap from "longdo-map-vue";
+import axios from "axios";
+import VueAxios from "vue-axios";
 
-Vue.config.productionTip = false
+Vue.use(VueAxios, axios);
 
+Vue.use(LongdoMap, {
+  load: {
+    apiKey: "2a6ae86c42d37ccd68ef6f52bc324493",
+  },
+});
+
+Vue.config.productionTip = false;
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
